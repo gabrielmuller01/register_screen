6fb4fa985fce192477759e38f274506e227a307d
Gabriel Muller <gabriel.polidoro@acad.ftec.com>
2022-11-08 20:34:48 -0300

    progress bar

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const passBar = document.querySelector(".password-strength div")
-const passLen = document.querySelector("#pass").value
-const count = 0
+let passwordBar = document.querySelector(".password-strength div")
+let count = 0;
 
-function passwordValidate(){
+function passwordValidate(pass){
+    
+    if (pass.length < 4){
 
-    if ((passLen.lenght >= 4) && (passLen.lenght <= 7)){
-        count += 5        
-    } else if (passLen.lenght > 7){
-        count += 5
+        passwordBar.setAttribute("style", "width: 0%")
+
+    }else if((pass.length >= 4) && (pass.length <= 7)){
+
+        count += 5 
+
+    }else if(pass.length > 7){
+
+        count += 10
+
     }
+
     
 }
 
-function 
+passwordValidate();
 
-// passBar.setAttribute("style", "width: 60%")
+
+// 8 letras 
+// 1 simbolo
+// 1 número 
+
+// passwordBar.setAttribute("style", "width: 20%")
+// passwordBar.style.backgroundColor = "red"
+// passwordBar.setAttribute("style", "width: 40%")
+// passwordBar.style.backgroundColor = "yellow"
